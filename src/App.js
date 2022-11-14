34cc6e4e7af4648cb1edda3225681edb8894b9ce
Husnain Aslam <114501183+HusnaiinASLAM@users.noreply.github.com>
2022-11-15 04:07:41 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React from 'react'
-import {Navbar,Menu} from './components'
+import {Menu,Navbar} from './components'
+import { Layout,Typography,Space } from 'antd'
+import { Route, Routes,Router } from "react-router";
+import { Link } from 'react-router-dom';
+import {Homepage,Exchanges,Cryptocurrencies,CryptoDetails,News} from './components'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-        <div className='Navbar'>
-            <Navbar/>
+    <div className='app'>
+        <div className='navbar'>
+            <Navbar></Navbar>
         </div>
-        <div className='MainMenu'>
+        <div className='main'>
+             <Layout>
+              
+              <Routes>
+                <Route exact path='/' element={<Homepage/>}/>
 
+                <Route path='/exchange' element={<Exchanges/>}/>
+                  
+                
+                <Route path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
+                
+                
+                <Route path='/crypto/:coinId' element={<CryptoDetails />}/>
+                  
+              
+                <Route path='news' element={<News />}/>
+                  
+               
+              </Routes>
+             </Layout>
+        
+        <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
         </div>
-        <div className='Footer'>
-
-        </div>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
-import { Typography } from 'antd';
+import { MoneyCollectOutlined,HomeOutlined,BulbOutlined,FundOutlined } from '@ant-design/icons';
+import { Typography,Menu } from 'antd';
 import { Avatar } from '@mui/material';
 import {Link } from 'react-router-dom'
 import img from '../images/cryptocurrency.png'
 
+
 const Navbar = () => {
   return (
-    <div className='navContainer'>
-        <div className='logoContainer'>
-        <Avatar src={img}/>
-            <Typography.Title>
+    <div className='nav-container'>
+        <div className='logo-container'>
+        <Avatar src={img} size='large'/>
+            <Typography.Title level={2} className='logo' >
              <Link to='/'>CryptoApp</Link>
             </Typography.Title>
-
         </div>
+       
         
+      <Menu theme='dark'>
+        <Menu.Item icon={<HomeOutlined/>}>
+          <Link to='/'>Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined/>}>
+          <Link to='/cryptocurrencies'>CryptoCurrency</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined/>}>
+          <Link to='/exchange'>Exchange</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined/>}>
+          <Link to='/news'>News</Link>
+        </Menu.Item>
+      </Menu>
         
     </div>
   )
